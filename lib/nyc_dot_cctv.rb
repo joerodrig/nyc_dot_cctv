d4096e63ec0898d95d9df300b94ca379043738b8
joerodrig <joerodrig3@gmail.com>
2019-02-21 21:22:00 -0500

    Ability to request an image from CCTV

--- lib/nyc_dot_cctv.rb
+++ lib/nyc_dot_cctv.rb
@@ -1,6 +1,22 @@
 require "nyc_dot_cctv/version"
+require 'net/http'
 
 module NycDotCctv
+  module_function
+
+  DEFAULT_CAMERA_IP = "207.251.86.238".freeze
+
   class Error < StandardError; end
-  # Your code goes here...
+
+  def request_image(cctv_id)
+    current_time = Time.now.utc
+    uri = URI("http://#{DEFAULT_CAMERA_IP}/cctv#{cctv_id}.jpg?rand=#{Random.rand}:80")
+    response = Net::HTTP.get_response(uri)
+    {
+      status_code: response.code,
+      body: response.body,
+      requested_at: current_time,
+      uri: uri
+    }
+  end
 end
