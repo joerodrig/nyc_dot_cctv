258bc0c59627600e1e9248b0741b41dacc8edccb
joerodrig <joerodrig3@gmail.com>
2019-02-24 14:33:09 -0500

    Add specs and abstract out logic

--- lib/nyc_dot_cctv.rb
+++ lib/nyc_dot_cctv.rb
@@ -4,19 +4,70 @@ require 'net/http'
 module NycDotCctv
   module_function
 
-  DEFAULT_CAMERA_IP = "207.251.86.238".freeze
-
   class Error < StandardError; end
+  DEFAULT_CAMERA_IP = "207.251.86.238".freeze
+  DEFAULT_OPTIONS = {
+    host: DEFAULT_CAMERA_IP,
+    img_type: "jpg".freeze,
+    port: "80".freeze
+  }
 
-  def request_image(cctv_id)
+  # @param cctv_id [String]
+  # @param custom_opts [Hash]
+  # @return [Hash]
+  def request_image(cctv_id, custom_opts: {})
     current_time = Time.now.utc
-    uri = URI("http://#{DEFAULT_CAMERA_IP}/cctv#{cctv_id}.jpg?rand=#{Random.rand}:80")
-    response = Net::HTTP.get_response(uri)
+    if invalid_cctv_id?(cctv_id)
+      return {
+        errors: ["An invalid CCTV ID was provided"],
+        requested_at: current_time
+      }
+    end
+
+    uri = generate_uri(cctv_id, {custom_opts: custom_opts})
+    res = Net::HTTP.get_response(uri)
     {
-      status_code: response.code,
-      body: response.body,
+      body: res.body,
       requested_at: current_time,
+      status_code: res.code,
       uri: uri
     }
+  end
+
+  # Generates a URI to request an image from a specified CCTV camera
+  # The default options should work in most cases, but can be overridden.
+  # @param cctv_id [String]
+  # @param custom_opts [Hash]
+  # @return [URI]
+  def generate_uri(cctv_id, custom_opts: {})
+    opts = DEFAULT_OPTIONS.merge(custom_opts)
+    uri = "http://#{cctv_host(opts[:host])}" \
+          "/cctv#{cctv_id}.#{opts[:img_type]}" \
+          "?rand=#{Random.rand}:#{opts[:port]}"
+    URI(uri)
+  end
+
+  # Specifies the cctv_host to use. If a valid host isn't
+  # specified, the default host is returned
+  # @param host [String]
+  # @return [Boolean]
+  def cctv_host(host)
+    return DEFAULT_CAMERA_IP if invalid_host?(host)
+    host
+  end
+
+  # A host is invalid if it's not a string, or if it's empty.
+  # example: host = "google.com"
+  # @param host [String]
+  # @return [Boolean]
+  def invalid_host?(host)
+    !host.class.to_s.eql?("String") || host.empty?
+  end
+
+  # Determine whether a provided cctv_id is invalid
+  # @param cctv_id [String]
+  # @return [Hash]
+  def invalid_cctv_id?(cctv_id)
+    invalid_host?(cctv_id)
   end
 end
--- spec/nyc_dot_cctv_spec.rb
+++ spec/nyc_dot_cctv_spec.rb
@@ -3,7 +3,148 @@ RSpec.describe NycDotCctv do
     expect(NycDotCctv::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "#request_image" do
+    before do
+      MockedTime = Struct.new(:utc)
+      allow(Time).to receive(:now).and_return(MockedTime.new(fake_time))
+    end
+
+    let(:fake_time) { 123 }
+
+    context "when an invalid cctv_id is provided" do
+      subject(:result) { described_class.request_image("") }
+
+      it "returns a hash containing an error" do
+        expect(result).to eq(
+          errors: ["An invalid CCTV ID was provided"],
+          requested_at: fake_time
+        )
+      end
+    end
+
+    context "when a valid cctv_id is provided" do
+      it "sends a GET request" do
+        MockedHttpGetResponse = Struct.new(:body, :code)
+        expect(Net::HTTP).to receive(:get_response)
+          .and_return(MockedHttpGetResponse.new({body: "", code: "200"}))
+        described_class.request_image("1")
+      end
+    end
+  end
+
+  describe "generate_uri" do
+    before do
+      allow(Random).to receive(:rand).and_return(mocked_random_num)
+    end
+
+    let(:mocked_random_num) { 0.12345 }
+    let(:cctv_id) { "1" }
+
+    context "when custom options aren't provided" do
+      subject(:result) { described_class.generate_uri(cctv_id) }
+
+      it "generates a URI with the default options" do
+        expect(result).to eq(URI(
+          "http://207.251.86.238" \
+          "/cctv#{cctv_id}.jpg" \
+          "?rand=#{mocked_random_num}:80"
+        ))
+      end
+    end
+
+    context "when a custom host is provided" do
+      subject(:result) { described_class.generate_uri(
+        cctv_id,
+        custom_opts: {host: custom_host})
+      }
+
+      let(:custom_host) { "google.com" }
+
+      it "generates a URI with the custom host and sets others to defaults" do
+
+        expect(result).to eq(URI(
+          "http://#{custom_host}" \
+          "/cctv#{cctv_id}.jpg" \
+          "?rand=#{mocked_random_num}:80"
+        ))
+      end
+    end
+
+    context "when a custom image type is provided" do
+      subject(:result) { described_class.generate_uri(
+        cctv_id,
+        custom_opts: {img_type: custom_image_type})
+      }
+
+      let(:custom_image_type) { "tiff" }
+
+      it "generates a URI with the custom host and sets others to defaults" do
+
+        expect(result).to eq(URI(
+          "http://207.251.86.238" \
+          "/cctv#{cctv_id}.#{custom_image_type}" \
+          "?rand=#{mocked_random_num}:80"
+        ))
+      end
+    end
+
+    context "when a custom port is provided" do
+      subject(:result) { described_class.generate_uri(
+        cctv_id,
+        custom_opts: {port: custom_port})
+      }
+
+      let(:custom_port) { "443" }
+
+      it "generates a URI with the custom host and sets others to defaults" do
+
+        expect(result).to eq(URI(
+          "http://207.251.86.238" \
+          "/cctv#{cctv_id}.jpg" \
+          "?rand=#{mocked_random_num}:#{custom_port}"
+        ))
+      end
+    end
+  end
+
+  describe "#cctv_host" do
+    let(:default_host) { described_class::DEFAULT_CAMERA_IP }
+
+    context "when a valid host isn't specified" do
+      subject(:result) { described_class.cctv_host("") }
+
+      it { is_expected.to eq(default_host) }
+    end
+
+    context "when a valid host is specified" do
+      subject(:result) { described_class.cctv_host(specified_host) }
+      let(:specified_host) { "google.com" }
+
+      it { is_expected.to eq(specified_host) }
+    end
+  end
+
+  describe "#invalid_host?" do
+    context "when the 'host' param is not a string" do
+      it "returns false" do
+        expect(described_class.invalid_host?({test: "123"})).to eq(true)
+        expect(described_class.invalid_host?(123)).to eq(true)
+        expect(described_class.invalid_host?(["123"])).to eq(true)
+      end
+    end
+
+    context "when the 'host' param is a string" do
+      context "when it is empty" do
+        subject(:result) { described_class.invalid_host?("") }
+
+        it { is_expected.to eq(true) }
+      end
+
+      context "when it is not empty" do
+        subject(:result) { described_class.invalid_host?("google.com") }
+
+        it { is_expected.to eq(false) }
+      end
+    end
   end
 end
